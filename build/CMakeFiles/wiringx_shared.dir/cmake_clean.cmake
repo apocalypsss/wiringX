dfc6c4b77318b5ba6997aa9b4fea913b010907e6
apocalypsss <frank@vhreve.nl>
2016-03-28 16:14:28 +0200

    Deleted build

--- build/CMakeFiles/wiringx_shared.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libwiringX.pdb"
-  "libwiringX.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/wiringx_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/wiringx_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libwiringX.pdb"
-  "libwiringX.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/wiringx_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
